6e701c5346fd16307ef272806e5e29fe97ed945a
David Wong Wan Ching <dwongwc@nexify.com.hk>
2022-03-21 12:31:37 +0800

    autoMapper test

---
+++ ConsoleApp1/Models/MappingTestModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    public class A
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class B
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class C
+    {
+        public int RowId { get; set; }
+        public string Name { get; set; }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutoMapper;
+using ConsoleApp1.Models;
+using System;
 
 namespace ConsoleApp1
 {
@@ -7,6 +9,45 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //AutoMapper Case 1: From A to B (all property are same) 
+            //var config = new MapperConfiguration(cfg => cfg.CreateMap<A, B>());
+            //var mapper = config.CreateMapper();
+            //var a = new A
+            //{
+            //    Id = 1,
+            //    Name = "Name A"
+            //};
+            //var b = mapper.Map<B>(a);
+
+            //AutoMapper Case 2: From A to B (a property is different) 
+            //var config = new MapperConfiguration(
+            //    cfg => cfg.CreateMap<A, C>().ForMember(x => x.RowId, y => y.MapFrom(o => o.Id))
+            //);
+            //var mapper = config.CreateMapper();
+            //var a = new A
+            //{
+            //    Id = 1,
+            //    Name = "Name A"
+            //};
+            //var c = mapper.Map<C>(a);
+
+
+            var config = new MapperConfiguration(
+                cfg => {
+                    cfg.CreateMap<A, B>();
+                    cfg.CreateMap<A, C>().ForMember(x => x.RowId, y => y.MapFrom(o => o.Id));
+                    }
+            );
+            var mapper = config.CreateMapper();
+            var a = new A
+            {
+                Id = 1,
+                Name = "Name A"
+            };
+            var b = mapper.Map<B>(a);
+            var c = mapper.Map<C>(a);
+
         }
     }
 }
